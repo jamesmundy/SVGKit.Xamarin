29f74a01234618d596e644daf0078140842cc8f3
James Mundy <james@foundbite.co>
2015-01-14 17:01:38 +0000

    Updated linker files.

--- SVGKitBindings/SVGKitBindings/libSVGKit-iOS.1.2.0.linkwith.cs
+++ SVGKitBindings/SVGKitBindings/libSVGKit-iOS.1.2.0.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libSVGKit-iOS.1.2.0.a", LinkTarget.ArmV7 | LinkTarget.Simulator, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libSVGKit-iOS.1.2.0.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true)]
