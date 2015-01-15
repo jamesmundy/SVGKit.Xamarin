2982dd562419a2266ce0ced2100cdcd2eaa007d7
James Mundy <james@foundbite.co>
2015-01-15 16:18:15 +0000

    Added Frameworks and linker flags

--- SVGKitBindings/SVGKitBindings/libSVGKit-iOS.1.2.0.linkwith.cs
+++ SVGKitBindings/SVGKitBindings/libSVGKit-iOS.1.2.0.linkwith.cs
@@ -1,4 +1,9 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libSVGKit-iOS.1.2.0.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libSVGKit-iOS.1.2.0.a", 
+	LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, 
+	SmartLink = true,
+	Frameworks="QuartzCore CoreText CoreGraphics CoreImage UIKit",
+	Linkerflags="-lxml2 -ObjC",
+	ForceLoad = true)]
